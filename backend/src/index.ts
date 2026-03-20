91b54d8a1cca5b8d6987046455552e967dd2a68b
khanyafreddie29 <khanyafreddie2903@gmail.com>
2026-03-20 09:49:23 +0200

    updated serverless function for vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,6 +26,17 @@ app.get('/health', (_, res) => {
 app.use('/api/venues', venueRoutes);
 app.use('/api/clock-ins', clockInRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on ${PORT}`)
+// });
+
+
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+// For Vercel serverless
+export default app;
